967c8a28dcad74041775e1ecf783cea754fffdca
Sophie Taylor <sophie@traumapony.org>
2012-07-26 20:59:42 +1000

    IDK WHAT IS GOING ON

---
+++ src/zephyrgc.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : zephyrgc.cpp
+// Author      : Sophie Taylor
+// Version     : wat
+// Copyright   : sup
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void ReadIMU(char* out,int &length)
+{
+	return;
+}
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	char* imuData = NULL;
+	int length = -1;
+	while(true)
+	{
+		 ReadIMU(imuData, length);
+	}
+
+}
