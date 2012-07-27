1c180022b1f85f5b963dc17b29614bf12023ea4c
Sophie Taylor <sophie@traumapony.org>
2012-07-27 12:39:14 +1000

    Added KinematicState struct; should move it into header but I'm too sleepy
    Signed-off-by: Sophie Taylor <sophie@traumapony.org>

--- src/zephyrgc.cpp
+++ src/zephyrgc.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+
 using namespace std;
 
 void ReadIMU(char* out,int &length)
@@ -14,13 +15,32 @@ void ReadIMU(char* out,int &length)
 	//Read serial from here
 	return;
 }
-
+struct KinematicState
+{
+	float X;
+	float Y;
+	float Z;
+	float Roll;
+	float Pitch;
+	float Yaw;
+	float Xdot;
+	float Ydot;
+	float Zdot;
+	float Rolldot;
+	float Pitchdot;
+	float Yawdot;
+};
 void Initialise(){}
 void ProcessEvents(){}
 void ActOnState(){}
+void ParseIMUData(KinematicState &state, char* imuData, int length)
+{
+
+}
 int main() {
 	cout << "Starting up the guidance :3" << endl;
 	Initialise();
+	KinematicState state;
 	while(true)
 	{
 		char* imuData = NULL;
@@ -37,6 +57,7 @@ int main() {
 		else
 		{
 			//Grab kinematic state from IMU here
+			ParseIMUData(state, imuData, length);
 		}
 
 		ProcessEvents();
